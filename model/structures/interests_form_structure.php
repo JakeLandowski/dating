bb849b7250e66fd2ae3a905cf552aa8c2f03c7b4
JakeLandowski <jakelandowski@gmail.com>
2018-02-01 10:48:01 -0800

    Finished aggregating interests form checkbox options into a data structure in model/structures/interests_form_structure.php

---
+++ model/structures/interests_form_structure.php
@@ -0,0 +1,17 @@
+<?php
+
+$indoor_options = 
+[
+    'tv' => 'Tv',                   'puzzles' => 'Puzzles',
+    'movies' => 'Movies',           'reading' => 'Reading',
+    'cooking' => 'Cooking',         'playing_cards' => 'Playing Cards',
+    'board_games' => 'Board Games',   'video_games' => 'Video Games'
+];
+
+$outdoor_options = 
+[
+    'hiking' => 'Hiking',           'walking' => 'Walking',
+    'biking' => 'Biking',           'climbing' => 'Climbing',
+    'swimming' => 'Swimming',       'tennis' => 'Tennis',
+    'collecting' => 'collecting',   'fishing' => 'Fishing'
+];
